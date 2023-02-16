a92e01bab4e2fda77b71e4c44b0c0708aa14bf1f
Raju Barde <rajub.forbearpro@gmail.com>
2023-02-16 15:19:23 +0530

    raju folder added

---
+++ src/raju/raju.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const raju = () => {
+  return (
+    <View>
+      <Text>raju</Text>
+    </View>
+  )
+}
+
+export default raju
